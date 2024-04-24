d46750cca7e7a1af971b63dfbee9e7c8b879dab0
Maganth Seetharaman <mahanthathreyee@gmail.com>
2024-04-24 12:04:49 -0700

    56 and 1239

---
+++ 56.py
@@ -0,0 +1,20 @@
+class Solution:
+    def merge(self, intervals: list[list[int]]) -> list[list[int]]:
+        res = []
+
+        start_idx = 0
+        end_idx = 1
+
+        for interval in sorted(intervals, key=lambda x: x[start_idx]):
+            if res and interval[start_idx] <= res[-1][end_idx]:
+                # Inside current interval
+                res[-1][end_idx] = max(res[-1][end_idx], interval[end_idx])
+            else:
+                res += interval,
+        
+        return res
+
+res = Solution().merge(
+    [[1,4],[1,5]]
+)
+print(res)
