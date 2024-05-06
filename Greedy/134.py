d07e034a18c206c7dbc9b2a8b46980afbf84f25b
Maganth Seetharaman <mahanthathreyee@gmail.com>
2024-05-06 13:22:54 -0700

    134 & 138

---
+++ Greedy/134.py
@@ -0,0 +1,30 @@
+'''
+Problem: Gas Station
+Link: https://leetcode.com/problems/gas-station/description/
+Tags: Greedy
+'''
+
+class Solution:
+    def canCompleteCircuit(self, gas: list[int], cost: list[int]) -> int:
+        n = len(gas)
+
+        surplus, total = 0, 0
+        res = 0
+
+        for i in range(n):
+            total += gas[i] - cost[i]
+            surplus += gas[i] - cost[i]
+
+            if surplus < 0:
+                surplus = 0
+                res = i + 1
+
+        if total >= 0:
+            return res
+        return -1
+
+res = Solution().canCompleteCircuit(
+    gas=[1,2,3,4,5],
+    cost=[3,4,5,1,2]
+)
+print(res)
---
+++ Linked List/138.py
@@ -0,0 +1,38 @@
+'''
+Problem: Copy List with Random Pointer
+Link: https://leetcode.com/problems/copy-list-with-random-pointer/
+Tags: Hashmap, LikedList
+'''
+
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+class Solution:
+    def copyRandomList(self, head):
+
+        # Insert each node's copy right after it, already copy .label
+        node = head
+        while node:
+            copy = Node(node.val)
+            copy.next = node.next
+            node.next = copy
+            node = copy.next
+
+        # Set each copy's .random
+        node = head
+        while node:
+            node.next.random = node.random and node.random.next
+            node = node.next.next
+
+        # Separate the copied list from the original, (re)setting every .next
+        node = head
+        copy = head_copy = head and head.next
+        while node:
+            node.next = node = copy.next
+            copy.next = copy = node and node.next
+
+        return head_copy
+
