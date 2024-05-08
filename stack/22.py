0528cdb8df17818894a50022b467fbb1082d9c2d
Maganth Seetharaman <mahanthathreyee@gmail.com>
2024-05-08 12:15:37 -0700

    133 & 28

---
+++ stack/22.py
@@ -0,0 +1,27 @@
+'''
+Problem: Generate Parentheses
+Link: https://leetcode.com/problems/generate-parentheses/description/
+Tags: Stack, String, Recursion
+'''
+
+class Solution:
+    def generateParenthesis(self, n: int) -> list[str]:
+        res = []
+
+        def solve(curr=str(''), o=0, c=0):
+            if o == c == n:
+                res.append(curr)
+                return
+            
+            if o <= n:
+                solve(curr+'(', o+1, c)
+            if c < o:
+                solve(curr+')', o, c+1)
+        
+        solve()
+        return res
+    
+res = Solution().generateParenthesis(
+    n=3
+)
+print(res)
