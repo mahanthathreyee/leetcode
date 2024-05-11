9132506b3eb7051bcd644dea01e95a3096fd971b
Maganth Seetharaman <mahanthathreyee@gmail.com>
2024-05-11 15:32:55 -0700

    33 & 79

---
+++ Binary Search/33.py
@@ -0,0 +1,27 @@
+'''
+Problem: Search in Rotated Sorted Array
+Link: https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+Tags: Binary Search, Array
+'''
+
+class Solution:
+    def search(self, A: list[int], target: int) -> int:
+        beg = 0
+        end = len(A)
+
+        while beg < end:
+            mid = (beg + end) // 2
+
+            if target < A[0] < A[mid]: beg = mid + 1
+            elif target >= A[0] > A[mid]: end = mid
+            elif target > A[mid]: beg = mid + 1
+            elif target < A[mid]: end = mid
+            else: return mid
+
+        return -1
+        
+res = Solution().search(
+    A=[4,5,6,7,0,1,2],
+    target=0
+)
+print(res)
