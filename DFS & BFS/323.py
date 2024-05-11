0f56fc2059942400766896af27ccd1e611ac92aa
Maganth Seetharaman <mahanthathreyee@gmail.com>
2024-05-11 15:28:51 -0700

    323 & 763

---
+++ DFS & BFS/323.py
@@ -0,0 +1,36 @@
+'''
+Problem: Number of Connected Components in an Undirected Graph
+Link: https://leetcode.com/problems/number-of-connected-components-in-an-undirected-graph/
+Tags: DFS, BFS, Components
+'''
+
+from collections import defaultdict
+
+class Solution:
+    def countComponents(self, n: int, edges: list[list[int]]) -> int:
+        adj_edge = defaultdict(list)
+        for x, y in edges:
+            adj_edge[x].append(y)
+            adj_edge[y].append(x)
+        
+        visited = set()
+        def dfs(root):
+            visited.add(root)
+
+            for node in adj_edge[root]:
+                if node not in visited:
+                    dfs(node)
+        
+        res = 0
+        for i in range(n):
+            if i not in visited:
+                dfs(i)
+                res += 1
+
+        return res
+
+res = Solution().countComponents(
+    n=6, 
+    edges=[[0,1], [1,2], [2,3], [4,5]]
+)
+print(res)
---
+++ Two Pointer/763.py
@@ -0,0 +1,25 @@
+'''
+Problem: Partition Labels
+Link: https://leetcode.com/problems/partition-labels/description/
+Tags: Two Pointer, Hash Table, String
+'''
+
+class Solution:
+    def partitionLabels(self, s: str) -> list[int]:
+        res = []
+        ch = {c: i for i, c in enumerate(s)}
+        
+        start = last = 0
+        for i, c in enumerate(s):
+            last = max(last, ch[c])
+            if last == i:
+                res.append(last - start + 1)
+                start = last + 1
+        
+        return res
+
+res = Solution().partitionLabels(
+    "ababcbacadefegdehijhklij"
+)
+
+print(res)
